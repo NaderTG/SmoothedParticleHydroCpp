807fc242d69d85b30392b2d10fcfe65211f51e66
antigsuit <ng1312@ic.ac.uk>
2016-06-05 15:25:20 +0100

    Initial Commit

---
+++ SPH/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SPH
+//
+//  Created by Nader on 05/06/2016.
+//  Copyright (c) 2016 Nader. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
